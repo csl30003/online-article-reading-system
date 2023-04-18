b92e32bf0a7d6ff554a291d63e9cf233ba62c428
离柒L <863577413@qq.com>
2023-04-18 19:51:46 +0800

    Initial commit

---
+++ src/main/java/com/example/eebighomework/EeBigHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.eebighomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EeBigHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EeBigHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eebighomework/EeBigHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eebighomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EeBigHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
