90a092c03ead240588294aa8c31a13815aa730a9
离柒L <863577413@qq.com>
2023-04-19 21:12:14 +0800

    完成model编写

---
+++ src/main/java/com/example/eebighomework/model/Article.java
@@ -0,0 +1,67 @@
+package com.example.eebighomework.model;
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ * 文章表
+ * </p>
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@TableName("article")
+public class Article implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 文章id
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 标题
+     */
+    private String title;
+
+    /**
+     * 内容
+     */
+    private String content;
+
+    /**
+     * 点赞数
+     */
+    private Integer likeCount;
+
+    /**
+     * 用户id
+     */
+    private Integer userId;
+
+    /**
+     * 上传时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime uploadTime;
+
+    /**
+     * 更新时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
+
+    /**
+     * 删除时间
+     */
+    private LocalDateTime deleteTime;
+
+}
